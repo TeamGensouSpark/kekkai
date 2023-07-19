ee91488d6ae18783d8ecf69252d946613311a46b
IAXRetailer <h2sxxa0w0@gmail.com>
2023-07-19 22:16:54 +0800

    expand dc character

---
+++ src/main/java/io/github/teamgensouspark/kekkai/character/TouhouExCharacter.java
@@ -0,0 +1,24 @@
+package io.github.teamgensouspark.kekkai.character;
+
+import net.katsstuff.teamnightclipse.danmakucore.entity.living.TouhouCharacter;
+import net.katsstuff.teamnightclipse.danmakucore.entity.living.TouhouSpecies;
+
+public class TouhouExCharacter {
+    // th18
+    public static final TouhouCharacter MIKE_GOUTOKUZI = TouhouCharacter.getOrCreate("MIKE_GOUTOKUZI", "mike",
+            TouhouSpecies.YOUKAI_BEAST_CAT);
+    public static final TouhouCharacter TAKANE_YAMASHIRO = TouhouCharacter.getOrCreate("TAKANE_YAMASHIRO", "takane",
+            TouhouExSpecies.YOUKAI_YAMAWARO);
+    public static final TouhouCharacter SANNYO_KOMAKUSA = TouhouCharacter.getOrCreate("SANNYO_KOMAKUSA", "sannyo",
+            TouhouExSpecies.YOUKAI_YAMAJAROU);
+    public static final TouhouCharacter MISUMARU_TAMATSUKURI = TouhouCharacter.getOrCreate("MISUMARU_TAMATSUKURI",
+            "misumaru", TouhouSpecies.GOD);
+    public static final TouhouCharacter TSUKASA_KUDAMAKI = TouhouCharacter.getOrCreate("TSUKASA_KUDAMAKI", "tsukasa",
+            TouhouSpecies.YOUKAI_BEAST_FOX);
+    public static final TouhouCharacter MEGUMU_IIZUNAMARU = TouhouCharacter.getOrCreate("MEGUMU_IIZUNAMARU", "megumu",
+            TouhouSpecies.YOUKAI_TENGU_CROW);
+    public static final TouhouCharacter CHIMATA_TENKYU = TouhouCharacter.getOrCreate("CHIMATA_TENKYU", "chimata",
+            TouhouSpecies.GOD);
+    public static final TouhouCharacter MOMOYO_HIMEMUSHI = TouhouCharacter.getOrCreate("MOMOYO_HIMEMUSHI", "momoyo",
+            TouhouExSpecies.YOUKAI_OOMUKADE);
+}
---
+++ src/main/java/io/github/teamgensouspark/kekkai/character/TouhouExSpecies.java
@@ -0,0 +1,12 @@
+package io.github.teamgensouspark.kekkai.character;
+
+import net.katsstuff.teamnightclipse.danmakucore.entity.living.TouhouSpecies;
+
+public class TouhouExSpecies {
+    public static final TouhouSpecies YOUKAI_YAMAWARO = TouhouSpecies.getOrCreate("YOUKAI_YAMAWARO",
+            TouhouSpecies.YOUKAI);
+    public static final TouhouSpecies YOUKAI_YAMAJAROU = TouhouSpecies.getOrCreate("YOUKAI_YAMAJAROU",
+            TouhouSpecies.YOUKAI);
+    public static final TouhouSpecies YOUKAI_OOMUKADE = TouhouSpecies.getOrCreate("YOUKAI_OOMUKADE",
+            TouhouSpecies.YOUKAI);
+}
