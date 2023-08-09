5f68bc61434b0e91b4bf38cb9a1735e9c42d7080
IAXRetailer <h2sxxa0w0@gmail.com>
2023-08-09 19:00:52 +0800

    config🔧: 1.1

--- src/main/java/io/github/teamgensouspark/kekkai/danmaku/subentity/impl/TrackingTargetSubEntity.java
+++ src/main/java/io/github/teamgensouspark/kekkai/danmaku/subentity/impl/TrackingTargetSubEntity.java
@@ -10,7 +10,13 @@ import net.minecraft.entity.EntityLivingBase;
 public class TrackingTargetSubEntity extends SubEntityDefault {
     @Override
     public DanmakuUpdate subEntityTick(DanmakuState danmakuState) {
-        EntityLivingBase target = KekkaiHelper.getSpellcardEntity(danmakuState.shot()).target().get();
+        EntityLivingBase target;
+        // BUG the server and client different performance here
+        try {
+            target = KekkaiHelper.getSpellcardEntity(danmakuState.shot()).target().get();
+        } catch (Exception e) {
+            return super.subEntityTick(danmakuState);
+        }
         Vector3 newDirection;
         if (target != null) {
             newDirection = (Vector3) Vector3.directionToPos(danmakuState.pos(), new Vector3(target));
