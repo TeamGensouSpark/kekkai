2081502dfed99b96309c3dae9cc31c86c4504a41
IAXRetailer <h2sxxa0w0@gmail.com>
2023-08-18 16:38:16 +0800

    🚀 v1.1.0

--- src/main/java/io/github/teamgensouspark/kekkai/KekkaiModInfo.java
+++ src/main/java/io/github/teamgensouspark/kekkai/KekkaiModInfo.java
@@ -2,7 +2,7 @@ package io.github.teamgensouspark.kekkai;
 
 public class KekkaiModInfo {
     public static final String MODID = "kekkai";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
     public static final String NAME = "Kekkai";
     
 }
